0d3607e7f0e81874a96ab7834e88e7899daf8bc2
t.sarker <tanmay.sarker@baruchmail.cuny.edu>
2020-07-08 15:02:37 -0400

    less questions more formatting

--- home.js
+++ home.js
@@ -1,47 +1,24 @@
-// setting up variables that have the values of questions
 var questions = [{
-    question: "1. What is the closest planet to the Sun?",
-    // The array are set up as multiple choice
-    choices: ["The Deathstar", "Jupiter", "Saturn", "Mercury"],
-    // Answer relative to the index of the array
+    question: "1. How do you write 'Hello World' in an alert box?",
+    choices: ["msg('Hello World')", "msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
     correctAnswer: 3
 }, {
-    question: "2. What is the great red spot on jupiter?",
-    choices: ["Grandma's House", "A Hawaiin Resort", "A storm", "A lake"],
+    question: "2. How to empty an array in JavaScript?",
+    choices: ["arrayList[]", "arrayList(0)", "arrayList.length=0", "arrayList.len(0)"],
     correctAnswer: 2
 }, {
-    question: "3. What is the sun mainly made of?",
-    choices: ["Cheese", "Hydrogen and helium gas", "Molten lava", "Liquid Iron"],
+    question: "3. What function to add an element at the begining of an array and one at the end?",
+    choices: ["push,unshift", "unshift,push", "first,push", "unshift,last"],
     correctAnswer: 1
 }, {
-    question: "4. Which of these is the smallest planet?",
-    choices: ["Mercury", "Jupiter", "Asteroid Belt", "Pluto"],
+    question: "4. What will this output? var a = [1, 2, 3]; console.log(a[6]);",
+    choices: ["undefined", "0", "prints nothing", "Syntax error"],
     correctAnswer: 0
 }, {
-    question: "5. What are comets mostly made of?",
-    choices: ["Ice and dust", "Water", "Apple Juice", "Fur"],
+    question: "5. What would following code return? console.log(typeof typeof 1);",
+    choices: ["string", "number", "Syntax error", "undefined"],
     correctAnswer: 0
-}, {
-    question: "6. How many moons does Mars have?",
-    choices: ["54", "2", "13", "12"],
-    correctAnswer: 1
-}, {
-    question: "7. Earth is located in which galaxy?",
-    choices: ["We are in a simulation", "Chocolate Milkshake Galaxy", "Andromeda Galaxy", "Milky way galaxy"],
-    correctAnswer: 3
-}, {
-    question: "8. What is the name of the force holding us to the Earth?",
-    choices: ["Fusion energy", "Taco tuesdays", "Gravity", "Chuck Norris"],
-    correctAnswer: 2
-}, {
-    question: "9. What is the name of NASA’s most famous space telescope?",
-    choices: ["Bubble", "Hubble", "Juggle", "Jungle"],
-    correctAnswer: 1
-}, {
-    question: "10.  What planet is famous for the beautiful rings that surround it?",
-    choices: ["Saturn", "Uranus", "Pluto", "Neptune"],
-    correctAnswer: 0
-}];
+}, ];
 
 
 var currentQuestion = 0;
